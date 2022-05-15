9ad57a0384f5d497c5cef519c535991f5a3119f8
u-nij <u.nij0509@gmail.com>
2022-05-15 23:23:51 +0900

    .

---
+++ Sorting/이코테_두배열의원소교체.py
@@ -0,0 +1,21 @@
+# 최대 K번의 바꿔치기 연산 : 배열 A 원소 하나와 B 원소 하나 골라 바꿔치기
+# 배열 A의 모든 원소의 합이 최대가 목표
+# => A에서 가장 원소 골라, B에서 가장 큰 원소와 교체
+
+import sys
+n, k = map(int, sys.stdin.readline().split()) # N과 K를 입력 받기
+a = list(map(int, sys.stdin.readline().split())) # 배열 A의 모든 원소를 입력받기
+b = list(map(int, sys.stdin.readline().split())) # 배열 B의 모든 원소를 입력받기
+
+a.sort()
+b.sort(reverse=True)
+
+for i in range(k):  # 두 배열의 원소를 최대 K번 비교
+    # A의 원소가 B의 원소보다 작은 경우
+    if a[i] < b[i]:
+        # 두 원소를 교체
+        a[i], b[i] = b[i], a[i]
+    else: # A의 원소가 B의 원소보다 크거나 같을 때, 반복문을 탈출
+        break
+
+print(sum(a))
